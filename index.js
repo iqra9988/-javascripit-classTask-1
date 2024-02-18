78a556962f9524ea15a244189adc0a5cf49f0f6f
iqra9988 <itsiqrarehman@gmail.com>
2024-02-18 22:48:54 +0500

    done my task

---
+++ index.js
@@ -0,0 +1,36 @@
+//Welcome message:
+// function displayWelcomMessage(){
+//   let userName = prompt("Enter your name");
+//  alert("welcom" + userName)}
+//  displayWelcomMessa ge()
+
+
+//Quote Generator:
+// Create a function called generateQuote that randomly selects and returns 
+// a quote from an array of predefined quotes. Call this function to
+//  display a random quote on the webpage.
+
+//     function QuoteGenerator(){
+//       quotes =[
+//     "Love conquers all",
+//     "Dream big dreams",
+//     "Stay true always",
+//     "Never give up"
+//    ];
+//    const randomindex = Math.floor(Math.random () * quotes.length);
+//    alert(quotes[randomindex])
+//     }
+//     QuoteGenerator()
+
+
+ 
+// Random Number Generator: 
+// Write a function called generateRandomNumber that generates and 
+// returns a random number between 1 and 100. Call this function and 
+// display the generated random number on the webpage.
+
+
+// function generateRandomNumber() {
+//       alert(Math.floor (Math.random()) *100);
+// }
+// generateRandomNumber()
